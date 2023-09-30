a6a51f80ffa6c11100229c9cdfecc77b34ce2fa5
Maddy Brown <brownm3@carleton.edu>
2023-09-30 16:47:02 -0500

    working on internet access

--- ProductionCode/cl_code.py
+++ ProductionCode/cl_code.py
@@ -13,11 +13,20 @@ def load_data():
             
     return rows
 
+
 def percentage_with_internet_access(country):
     """Takes a country and returns a number representing the 
     percentage of people from that country with internet access"""
+    country_list = []
+    data = load_data()
+    for row in data:
+        # group by country
+        if row[0] == country:
+            country_list.append(row)
+    print(country_list)
+#country = sys.argv[1]
+load_data()
     
-    return
 
     
     
