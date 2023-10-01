1b590618bde0e5594a7fd5fbf2119e5da1d3a831
albertezem <ezemosakwea@carleton.edu>
2023-09-30 21:15:00 -0500

    added some tests and refactored

--- ProductionCode/cl_code.py
+++ ProductionCode/cl_code.py
@@ -8,10 +8,11 @@ def load_data():
     
     with open('Data/world_bank.csv', "r") as file:
         reader = csv.reader(file)
+        next(reader)
         for row in reader:
             rows.append(row)
             
-    return rows
+    return rows    
 
 def load_header():
     """Loads the column names and returns them as a list"""
@@ -22,33 +23,65 @@ def load_header():
 
     return column_names
 
-def group_by_country(country): 
-    country_list = []
-    for row in data:
-        if row[0] == country:
-            country_list.append(row)
-    return country_list
-def group_by_internet_access(country_list): 
-    internet_list = []
+def get_column_index(column_name):
     column_names = load_header()
-    col = column_names.index("internetaccess")
-    for row in country_list:
-        internet_list.append(row[col])
-    return internet_list
-def percentage_with_internet_access(country):
-    yes_internet_access = "1"
-    country_list = group_by_country(country)
-    internet_list = group_by_internet_access(country_list)
-    percentage = (internet_list.count(yes_internet_access)/len(internet_list)) * 100
+    idx = column_names.index(column_name)
+    
+    return idx
+    
+
+def filter(by, col, data):
+    """Takes a keyword, a column name, and a dataset, and
+    returns the portion of the dataset that matches the keyword 
+    in the given column as a list"""
+    new_data = []
+    idx = get_column_index(col)
+    
+    for row in data:
+        
+        if row[idx] == by:
+            new_data.append(row)
+
+    return new_data
+
+def get_column(column_name, data):
+    """Takes a dataset and a column name, and returns the column as a list""" 
+    column = []
+    idx = get_column_index(column_name)
+    
+    for row in data:
+        column.append(row[idx])
+        
+    return column
+    
+def percentage_with_internet_access(country, data):
+    """Takes a country name and a dataset, and returns the
+    percentage of people that have access to the internet"""
+    
+    has_internet_access = "1"
+    
+    country_data = filter(country, "economy", data)
+    
+    internet_column = get_column("internetaccess", country_data)
+    
+    num = internet_column.count(has_internet_access)
+    total = len(internet_column)
+    
+    percentage = (num / total) * 100
+    
     return percentage
 
-#put the sys argv stuff in a main function
-country = sys.argv[1]
-data = load_data()
-print(percentage_with_internet_access(country))
+def main():
+    #put the sys argv stuff in a main function
+    #country = sys.argv[1]
+    
+    data = load_data()
+    print(percentage_with_internet_access("United States", data))
+
+main()
 
 
-print(load_data())
+#print(load_data())
 
     
     
--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -5,15 +5,62 @@ from ProductionCode.cl_code import *
 class test_dataset(unittest.TestCase):
     
     def setUp(self):
-        load_data()
+        data = load_data()
     
-    def test_percent_internet_access(self):
-        """Given an existing country, should return a value """
+    def test_percent_internet_access1(self):
+        """Given an existing country, percentage_with_internet_access returns the correct value"""
         
         country = "Afghanistan"
-        ratio = percentage_with_internet_access(country)
+        ratio = percentage_with_internet_access(country, data)
         
         self.assertAlmostEqual(ratio, 19.7)
+        
+    def test_percent_internet_access2(self):
+        """Given an existing country, percentage_with_internet_access returns the correct value"""
+        
+        country = "Nigeria"
+        ratio = percentage_with_internet_access(country, data)
+        
+        self.assertAlmostEqual(ratio, 41.6)
+        
+    def test_load_header(self):
+        """Given the dataset, load_header correctly loads the header"""
+        header = load_header()
+        
+        self.assertEqual(header[-1], "year")
+    
+    def test_get_column(self):
+        """Given a column name and a dataset, get_column correctly returns the
+        column"""
+    
+    def test_filter(self):
+        """Given a keyword, column, and dataset, filter correctly returns
+        the portion of the dataset where the column value matches the keyword"""
+        
+        key = "GEO"
+        col_name = "economycode"
+        
+        new_data = filter(key, col_name, data)
+        
+    
+    
+    def test_get_column_index(self):
+        """Given a column name and a dataset, correctly returns the column's name"""
+        
+        
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
 
     
