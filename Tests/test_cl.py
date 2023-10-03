9e0957b1e871c8dafdbb589d4378f17f0c8eb74f
Maddy Brown <brownm3@carleton.edu>
2023-10-02 21:10:49 -0500

    updating

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -71,6 +71,7 @@ class test_dataset(unittest.TestCase):
         col_name = "Group E"
         key = "Albert"
         
+        self.assertEqual
         self.assertRaises(ValueError, filter, key, col_name, self.data) # make it print out a correction statement instead of throwing an error
     
     def test_get_column_index(self):
@@ -88,26 +89,23 @@ class test_dataset(unittest.TestCase):
         
         educ_levels = education_level_by_gender(country, self.data)
         
-        # For some reason, need to print result to pass the test. Ask about later.
-        #print(educ_levels)
-        
         self.assertEqual(educ_levels[0][0][1], 29.11)
         
         
         
-    """def test_main(self):
-        code = subprocess.Popen(["python3", "-u", "ProductionCode/cl_code.py", "--internet_access_by_country" "Algeria"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf8')
+    def test_main(self):
+        code = subprocess.Popen(["python3", "-u", "ProductionCode/cl_code.py", "--internet_access_by_country", "Algeria"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf8')
         output, err = code.communicate()
         self.assertEqual(output.strip(), "88.9 percent of Algeria has internet access.")
         code.terminate()
-        """
     
-    """def test_main_2(self):
+    
+    def test_main_2(self):
         code = subprocess.Popen(["python3", "-u", "ProductionCode/cl_code.py", "--education_levels_by_country_and_gender", "Peru"], stdin = subprocess.PIPE, stdout = subprocess.PIPE, encoding = 'utf8')
         output, err = code.communicate()
         self.assertEqual(output.strip(), "Education levels in Peru:\nFor females:\nPrimary school or less: 16.94 percent\nSecondary school: 74.09 percent\nTertiary education or more: 8.47 percent\nFor males:\nPrimary school or less: 13.57 percent\nSecondary school: 74.62 percent\nTertiary education or more: 11.56 percent")
         code.terminate()
-        """
+    
         
 
 if __name__ == '__main__':
