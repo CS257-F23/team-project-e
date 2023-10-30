aef9224c60e9e0c031c68f6ccd29d34f1d9e20b3
albertezem <ezemosakwea@carleton.edu>
2023-10-29 22:09:21 -0500

    csv files properly formatted

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,20 +1,19 @@
 DROP TABLE IF EXISTS countries;
 CREATE TABLE countries (
+    id int,
     country text,
     country_abbreviation text,
     country_region text,
-    id serial,
-)
-
+    adult_population text
+);
 DROP TABLE IF EXISTS poll_results;
 CREATE TABLE poll_results (
-    adult_population text,
+    id int,
     age text,
     education_level text,
     employment_status text,
     financial_account_status text,
     worry_about_financing_education text,
     internet_acess text,
-    id serial,
     country_id int
 )
