779feaadeb14ecb7c1e4495d60a69665ca8b2710
Maddy Brown <brownm3@carleton.edu>
2023-10-31 16:50:00 -0400

    fixed spelling error with internet_access variable in poll_results table

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -14,6 +14,6 @@ CREATE TABLE poll_results (
     employment_status text,
     financial_account_status text,
     worry_about_financing_education text,
-    internet_acess text,
+    internet_access text,
     country_id text
 )
