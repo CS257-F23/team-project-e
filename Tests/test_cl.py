9891c9c90348444c332767050fe111c7b85c5753
--unset <ezemosakwea@carleton.edu>
2023-11-01 21:56:39 -0400

    removed redundant tests

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -30,87 +30,6 @@ class TestDataset(unittest.TestCase):
         keyword_validity = self.data.check_keyword_validity(keyword, column, self.data)
 
         self.assertEqual(keyword_validity, False)
-    
-    def test_check_column_validity(self):
-        """Test checking that check_column_validity returns True for a valid input"""
-
-        column = "educ"
-
-        column_validity = self.data.check_column_validity(column)
-
-        self.assertEqual(column_validity, True)
-
-    def test_check_column_validity_edge_case(self):
-        """Test checking that check_column_validity returns False for an invalid input"""
-
-        column = "hemisphere"
-        column_validity = self.data.check_column_validity(column)
-
-        self.assertEqual(column_validity, False)
-
-    def test_get_column(self):
-        """Given a column name and a dataset, get_column returns the
-        column as a string"""
-
-        column_name = "regionwb"
-        column = self.data.get_column(column_name, self.data.connect())
-
-        self.assertEqual(column[0], "South Asia")
-    
-    def test_get_column_edge_case(self):
-        """Edge case testing that get_column correctly identifies that an invalid column string
-        has been given"""
-
-        column_name = "happiness"
-        column = self.data.get_column(column_name, self.data.connect())
-        message = "Invalid column name."
-
-        self.assertEqual(column, message)
-    
-    def test_filter(self):
-        """Given a keyword, column, and dataset, filter returns
-        the portion of the dataset where the column value matches the keyword string"""
-        
-        key = "GEO"
-        col_name = "economycode"
-        
-        new_data = self.data.filter(key, col_name)
-
-        self.assertEqual(new_data[2][0], "Georgia")
-    
-    def test_filter_edge_case(self):
-        """Edge case testing that an invalid key for filter() correctly identifies
-        an invalid key string"""
-
-        key = "HEY"
-        col_name = "economycode"
-        message = "Invalid keyword or column name."
-
-        new_data = self.data.filter(key, col_name)
-
-        self.assertEqual(new_data, message)
-        
-    def test_get_ratio_of_key_in_column(self):
-        """Given a key and column, get_ratio returns how often the key appears
-        in the column as a integer ratio"""
-        
-        column = [1, 1, 2, 2]
-        key = 1
-        
-        rate = self.data.get_ratio_of_key_in_column(key, column)
-        
-        self.assertEqual(rate, 50)
-    
-    def test_get_ratio_of_key_in_column_edge_case(self):
-        """Test checking the edge case in which an incorrect key name string 
-        has been given. Should return a 0.0 integer"""
-
-        key = "Chicago"
-        column = "economy"
-
-        rate = self.data.get_ratio_of_key_in_column(key, column)
-
-        self.assertEqual(rate, 0.0)
 
     def test_list_of_countries(self):
         """Testing if the function correctly returns the list of countries
@@ -130,50 +49,6 @@ class TestDataset(unittest.TestCase):
         result = "Afghanistan"
 
         self.assertEqual(result, part_string)
-    
-
-    def test_get_average_of_column(self):
-        """Given a column of integers, returns the mean as an integer"""
-        
-        country = "Argentina"
-        column = "age"
-        
-        average_age = self.data.get_average_of_column(country, column)
-        
-        self.assertEqual(average_age, 45.6) 
-
-    def test_get_average_of_column_edge_case(self):
-        """Test identifying whether get_average_of_column will correclty identify when an 
-        incorrect country string has been given"""
-        self.maxDiff = None
-
-        column = "age"
-        country = "Canad"
-        message = "python3 cl_code.py --function <function_name> --country <country_name> \
-            \nFunction options:\nfour_stat_summary\nfinancial_account_comparison\nage_education_worry_comparison\nCountry options: \
-            \nHint: If the country is multiple words long, enclose the name in quotes.\n" + self.data.string_of_countries() + "To view this information at any time, type 'python3 cl_code.py -h' in the command line."
-        average_age = self.data.get_average_of_column(country, column)
-
-        self.assertEqual(average_age, message)
-    
-    def test_calculate_averages(self):
-        """Test identifying whether calculate_averages correctly calculates averages of integers
-        Should output an integer"""
-
-        test_data = [1, 2, 3, 4]
-        average = self.data.calculate_averages(test_data)
-        result = 2.5
-
-        self.assertEqual(average, result)
-    
-    def test_calculate_averages_edge_case(self):
-        """Test identifying whether calculate_averages will correctly return message if no data is given"""
-
-        test_data = []
-        average = self.data.calculate_averages(test_data)
-        result = "Cannot calculate the average of no data."
-
-        self.assertEqual(average, result)
     
     def test_has_financial_account_single_country(self):
         """Tests that has_financial_account_single_country correctly returns the percentage of a 
