f65114833789c5de6a25820a7cda6b25c2369754
solomonh <solomonh@stearns.mathcs.carleton.edu>
2023-11-09 19:42:19 -0500

    updated cl_code

--- cl_code.py
+++ cl_code.py
@@ -6,11 +6,11 @@ def argument_parser():
         input: list [data] 
         output: string (arguments)"""
     dataset = Dataset()
-    parser = argparse.ArgumentParser(usage = dataset.usage_statement())
+    parser = argparse.ArgumentParser(usage = dataset.get_usage_statement())
     parser.add_argument("--function", type = str, help = "Usage: python3 ProductionCode/cl_code.py --function <function_name> \
                         --country <country_name>\nFunction options:\nfour_stat_summary, financial_account_comparison, age_education_worry_comparison") 
     parser.add_argument("--country", type = str, help = "Country options:\nHint: If the country is multiple words long, \
-                        enclose the name in quotes.\n" + dataset.string_of_countries())
+                        enclose the name in quotes.\n" + dataset.get_string_of_countries())
     arguments = parser.parse_args()
 
     return arguments
@@ -22,19 +22,19 @@ def function_argument_choice(arguments, data):
     """
 
     if arguments.function == "four_stat_summary":
-        four_main_stats_of_interest = data.format_four_stat_summary_by_country(arguments.country)
+        four_main_stats_of_interest = data.get_formatted_four_stat_summary_by_country(arguments.country)
         return four_main_stats_of_interest
     
     elif arguments.function == "financial_account_comparison":
-        financial_comparison_results = data.format_financial_comparison(arguments.country)
+        financial_comparison_results = data.get_formatted_financial_comparison(arguments.country)
         return financial_comparison_results
         
     elif arguments.function == "age_education_worry_comparison":
-        age_education_results = data.format_age_financial_worry_by_education_summary(arguments.country)
+        age_education_results = data.get_formatted_age_financial_worry_by_education_summary(arguments.country)
         return age_education_results
 
     else:
-        usage_statement_message = data.usage_statement()
+        usage_statement_message = data.get_usage_statement()
         return usage_statement_message
 
 
@@ -48,7 +48,7 @@ def main():
         
     arguments = argument_parser()
 
-    country_validity = data.check_keyword_validity(arguments.country)
+    country_validity = data.get_country_validity(arguments.country)
 
     if country_validity == True:
 
@@ -56,7 +56,7 @@ def main():
         print(results)
         
     else:
-        usage_statement_message = data.usage_statement()
+        usage_statement_message = data.get_usage_statement()
         print(usage_statement_message)
 
         
