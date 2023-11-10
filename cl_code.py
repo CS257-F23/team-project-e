3b9108c083e8799544c4d4b9a23bf8ee033f4a98
Maddy Brown <brownm3@carleton.edu>
2023-11-09 19:53:32 -0500

    pulling

--- cl_code.py
+++ cl_code.py
@@ -2,65 +2,60 @@ import argparse
 from ProductionCode.core import *
     
 def argument_parser():
-    """ Returns the argument that is inputed into the terminal by the user
-        input: list [data] 
-        output: string (arguments)"""
-    dataset = Dataset()
-    parser = argparse.ArgumentParser(usage = dataset.usage_statement())
+    """Returns the argument that is inputed into the terminal by the user.
+        Input: None 
+        output: command line arguments (string)"""
+    parser = argparse.ArgumentParser(usage = data.get_usage_statement())
     parser.add_argument("--function", type = str, help = "Usage: python3 ProductionCode/cl_code.py --function <function_name> \
                         --country <country_name>\nFunction options:\nfour_stat_summary, financial_account_comparison, age_education_worry_comparison") 
     parser.add_argument("--country", type = str, help = "Country options:\nHint: If the country is multiple words long, \
-                        enclose the name in quotes.\n" + dataset.string_of_countries())
+                        enclose the name in quotes.\n" + data.get_string_of_countries())
     arguments = parser.parse_args()
 
     return arguments
 
-def function_argument_choice(arguments, data):
-    """ Returns the data result associated with the specifc argument the user types into the terminal.
-    input: arguments(string), list[data]
-    output: four_main_stats_of_interest(string) , or financial_comparison_results (string), or age_education_results (string), or usage statement(string)
+def function_argument_choice(arguments):
+    """ Returns the result associated with the specifc argument the user types into the terminal.
+    Input: command line arguments (string)
+    Returns: results of the given function (string)
     """
 
     if arguments.function == "four_stat_summary":
-        four_main_stats_of_interest = data.format_four_stat_summary_by_country(arguments.country)
+        four_main_stats_of_interest = data.get_formatted_four_stat_summary_by_country(arguments.country)
         return four_main_stats_of_interest
     
     elif arguments.function == "financial_account_comparison":
-        financial_comparison_results = data.format_financial_comparison(arguments.country)
+        financial_comparison_results = data.get_formatted_financial_comparison(arguments.country)
         return financial_comparison_results
         
     elif arguments.function == "age_education_worry_comparison":
-        age_education_results = data.format_age_financial_worry_by_education_summary(arguments.country)
+        age_education_results = data.get_formatted_age_financial_worry_by_education_summary(arguments.country)
         return age_education_results
 
     else:
-        usage_statement_message = data.usage_statement()
+        usage_statement_message = data.get_usage_statement()
         return usage_statement_message
-
-
 
 def main():
     """Loads the data, parses the command line, and prints the results of the specificed command line function.
-    Output: the data associated with the argument associated with the command line"""
-
-    data = Dataset()
-    data.connect()
+    Input: None
+    Output: the results associated with the given arguments (string)"""
         
     arguments = argument_parser()
 
-    country_validity = data.check_keyword_validity(arguments.country)
-
+    country_validity = data.get_country_validity(arguments.country)
     if country_validity == True:
-
-        results = function_argument_choice(arguments, data)
+        results = function_argument_choice(arguments)
         print(results)
         
     else:
-        usage_statement_message = data.usage_statement()
+        usage_statement_message = data.get_usage_statement()
         print(usage_statement_message)
 
         
 if __name__ == "__main__":
+    data = Dataset()
+    data.connect()
     main()
 
 
